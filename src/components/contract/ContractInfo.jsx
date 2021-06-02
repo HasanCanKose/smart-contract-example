264c2717673511d649ce621a4e28e83c1a897902
Hasan Can Kose <hasancannkosee@gmail.com>
2021-06-02 05:13:56 +0300

    Add date time

--- src/components/contract/ContractInfo.jsx
+++ src/components/contract/ContractInfo.jsx
@@ -1,13 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ContractContext from "../../context/contract/contractContext";
 
 const ContractInfo = () => {
   const contractContext = useContext(ContractContext);
   const { contractInfo } = contractContext;
+
+  const [time, setTime] = useState(null);
+
+  useEffect(() => {
+    if (contractInfo) {
+      var date = new Date().getTime();
+      setTime(currentTime(date));
+    }
+  }, [contractInfo]);
+
+  const currentTime = (currTime) => {
+    var options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    };
+    return new Date(currTime).toLocaleDateString("tr-TR", options);
+  };
   return (
     <div>
       <p>{JSON.stringify(contractInfo)}</p>
       <p>{contractInfo && contractInfo.customer}</p>
+      <p>{contractInfo && time}</p>
+      <p>{contractInfo && contractInfo.orderTime}</p>
+      asdasd
     </div>
   );
 };
